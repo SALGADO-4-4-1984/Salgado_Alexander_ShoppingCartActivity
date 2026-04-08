bf8d7a5d3b6e04dd5f902c2bb5704a073433bb3a
Alexander B. Salgado <djthugsalgado@gmail.com>
2026-04-09 01:29:29 +0800

    Added Section 1 Product class
    Includes Product class with fields, DisplayProduct(), 
    and supporting methods. 
    
    Code is organized with:
    //------SECTION NO. (TITLE OF SPECIFIC PROGRAM FUNCTION)-----//
    comments for readability and structure.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿//----------------------------------------------SECTION 1: ( PRODUCT CLASS )--------------------------------------------------------//
+using System;
+
+class Product
+{
+    public int Id;
+    public string Name;
+    public double Price;
+    public int RemainingStock;
+
+    public void DisplayProduct()
+    {
+        Console.WriteLine($"ID: {Id}, Name: {Name}, Price: {Price}, Stock: {RemainingStock}");
+    }
+
+    public double GetItemtotal(int quantity)
+    {
+        return Price * quantity;
+    }
+
+    public bool HasEnoughStock(int quantity)
+    {
+        if (RemainingStock >= quantity)
+        {
+            return true; // When stock is available
+        }
+        else
+        {
+            return false; // Not Enough Stock
+        }
+    }
+
+
+    public void DeductStock(int quantity) // Reduce stock or -1 stock per purchase of product
+    {
+        RemainingStock -= quantity;
+    }
+
+}
+
+//----------------------------------------------------------------DEAD END---------------------------------------------------------//
